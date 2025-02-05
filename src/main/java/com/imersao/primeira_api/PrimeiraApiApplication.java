24dfb20d9ed6125286c986471817ca534cb74e1e
Thiago Pereira <thiagopnascimento11@gmail.com>
2025-02-05 20:06:52 -0300

    Código inicial 2

--- src/main/java/com/imersao/primeira_api/PrimeiraApiApplication.java
+++ src/main/java/com/imersao/primeira_api/PrimeiraApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PrimeiraApiApplication {
 
 	public static void main(String[] args) {
+		//teste
 		SpringApplication.run(PrimeiraApiApplication.class, args);
 	}
 
